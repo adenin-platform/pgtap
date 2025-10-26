bb8376daed55661722653c317b3e7a4b291765e8
Martin Amm <IntranetFactory@users.noreply.github.com>
2025-10-26 07:33:44 -0400

    Add generated pgtap.sql file

---
+++ sql/pgtap.sql
@@ -0,0 +1,9 @@
+-- This is an automatically generated file. Do not edit directly.
+
+-- Replace MODULE_PATHNAME with $libdir/pgtap
+-- Replace __OS__ with Linux
+-- Replace __VERSION__ with 1.3.5
+
+CREATE EXTENSION pgtap;
+
+-- Other necessary SQL commands can be added here.
